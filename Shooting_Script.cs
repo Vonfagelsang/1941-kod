9868b014ee1fddb4b8ac0c95401d3739ee18a720
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-11-09 12:00:17 +0100

    Add files via upload

--- Shooting_Script.cs
+++ Shooting_Script.cs
@@ -19,7 +19,7 @@ public class Shooting_Script : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
             Rigidbody clone;
             clone = Instantiate(projectile, new Vector3(-1.51f, 1.23f, -7f), transform.rotation);
-            clone.velocity = new Vector3(0, 10, 0);
+            clone.velocity = new Vector3(0, 0, 50);
         }
     }
 }
